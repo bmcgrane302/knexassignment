83c9f77f07dcbdfdb8d7d895cbcaa872e366c548
bmcgrane302 <bmcgrane302@yahoo.com>
2017-10-13 14:58:07 -0700

    step 3

---
+++ server.js
@@ -0,0 +1,55 @@
+const env = 'development';
+const config = require('./knexfile.js')[env];
+const knex = require('knex')(config);
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const port = process.env.PORT || 8000;
+app.use(bodyParser.json({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get('/users', function(req, res) {
+  knex('users').then((result) => {
+    //console.log(result)
+    res.render('users',{userResults: result});
+  })
+  .catch((err) => {
+    console.error(err)
+  });
+});
+
+app.get('/profile/:id', function(req, res) {
+  knex('users')
+    .where('id', req.params.id)
+    .limit(1)
+    .then((result)=>{
+      console.log(result[0]);
+      res.render('profile',{userResults: result[0]});
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
+});
+
+app.get('/edit/:id', function(req, res) {
+  knex('users')
+    .where('id', req.params.id)
+    .limit(1)
+    .then((result)=>{
+      console.log(result[0]);
+      res.render('edit', {userResults: result[0]});
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
+});
+
+
+
+
+
+app.listen(port, function() {
+  console.log('Listening on', port);
+});
